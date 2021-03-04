e1d4c04d6e10f62cf4c1414d061c4059d7f22796
bsoviedoy <63557478+bsoviedoy@users.noreply.github.com>
2021-03-04 15:13:47 -0500

    Add files via upload

--- GEOVISOR/JS/main.js
+++ GEOVISOR/JS/main.js
@@ -11,7 +11,9 @@ var base= L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',).addTo(map);
 //Esta versión se hace con marker cluster
 
 
-var markers = L.markerClusterGroup(); //Se establece la variable  markers que sera en esencia
+var markers = L.markerClusterGroup({
+  attribution: 'Twitter/ Vidas silenciadas'
+}); //Se establece la variable  markers que sera en esencia
 //un array de los datos que se le pongan. Acá se pone vacia
 
 var geoJsonLayer = L.geoJson(data, { //acá se lee el layer del geojson
