b51e61174b59f61d4ccbdd00d4e9b54476d2a6c8
bsoviedoy <63557478+bsoviedoy@users.noreply.github.com>
2021-04-11 18:31:21 -0500

    v.2 actualización popup

--- GEOVISOR/JS/main.js
+++ GEOVISOR/JS/main.js
@@ -8,6 +8,7 @@ var map =  L.map('mapid', {
   
 var base= L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',).addTo(map);
 
+
 //Esta versión se hace con marker cluster
 
 
@@ -18,11 +19,13 @@ var markers = L.markerClusterGroup({
 
 var geoJsonLayer = L.geoJson(data, { //acá se lee el layer del geojson
   onEachFeature: function (feature, layer) { //Y se establece la función para desplegar el popup
-    layer.bindPopup(feature.properties.Desc); // Se le dice: cuando se haga clic, se despliega esa información
-  }
+    layer.bindPopup("<h2> Detalles: </h2> <table class= 'table' border='1'> <tr> <td> <strong>Municipio: </strong> </td> <td>" + feature.properties.Municipio+ "</td> </tr>"+
+    "<br> <br>"+ "<tr> <td> <strong>Descripción: </strong> </td> <td>" + feature.properties.Desc + "<br> </td> </tr> </table>" ); // Se le dice: cuando se haga clic, se despliega esa información
+  }// la función .bindPopup permite el uso de HTML, por lo que se puede personalizar lo que sale en cada dato.
 });
 markers.addLayer(geoJsonLayer);  //y acá se añade cada punto a la variable markers
 
 
 map.addLayer(markers);  // Se añade la variable al mapa
 map.fitBounds(markers.getBounds()); // Y esta si no se para que es :v
+
